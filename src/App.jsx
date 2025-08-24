f7e3cf4d24a764f7c49a972eaadc2826f149776c
Abhishek <abhishek.kumar7324046@gmail.com>
2025-08-24 11:30:57 +0530

    Update project name - trigger redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ ChartJS.register(
 
 const STORAGE_KEY = "trading_journal_trades_v1";
 
-// Money sound function
+// Money sound function - Updated for new project name
 function playSuccessSound() {
   try {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
